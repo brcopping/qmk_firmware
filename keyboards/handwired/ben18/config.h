1d2f00d59ebf62298ff3bb645bf460fbe4238cf5
Ben Copping <b.copping@sportradar.com>
2019-12-08 10:49:33 +0000

    numpad

--- keyboards/handwired/ben18/config.h
+++ keyboards/handwired/ben18/config.h
@@ -7,9 +7,9 @@
 #define VENDOR_ID       0xFEED
 #define PRODUCT_ID      0x6060
 #define DEVICE_VER      0x0001
-#define MANUFACTURER    qmkbuilder
-#define PRODUCT         keyboard
-#define DESCRIPTION     Keyboard
+#define MANUFACTURER    bcopping
+#define PRODUCT         BPad
+#define DESCRIPTION     a simple numpad
 
 /* key matrix size */
 #define MATRIX_ROWS 5
--- keyboards/handwired/ben18/kb.h
+++ keyboards/handwired/ben18/kb.h
@@ -1,20 +1,17 @@
-#ifndef KB_H
-#define KB_H
+#pragma once
 
 #include "quantum.h"
 
-#define KEYMAP( \
+#define LAYOUT( \
 	K00, K01, K02, K03, \
-	K10, K11, K12, K13, \
-	K20, K21, K22,      \
-	K30, K31, K32, K33, \
-	K40,      K42  \
+	K10, K11, K12,      \
+	K20, K21, K22, K23, \
+	K30, K31, K32,      \
+	K40,      K42, K43  \
 ) { \
-	{ K00,   K01,   K02,   K03 }, \
-	{ K10,   K11,   K12,   K13 }, \
-	{ K20,   K21,   K22,   KC_NO }, \
-	{ K30,   K31,   K32,   K33 }, \
-	{ K40,   KC_NO, K42,   KC_NO }  \
+	{ K00,   K01,   K02,   K03   }, \
+	{ K10,   K11,   K12,   KC_NO }, \
+	{ K20,   K21,   K22,   K23   }, \
+	{ K30,   K31,   K32,   KC_NO }, \
+	{ K40,   KC_NO, K42,   K43   }  \
 }
-
-#endif
